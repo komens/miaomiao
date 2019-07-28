76168c39993c4519a2cde56d8b066037dac7a487
YoungAm <youngam@qq.com>
2019-07-28 15:20:24 +0800

    添加动态数据

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -4,94 +4,104 @@
          <div class="city_hot">
             <h2>热门城市</h2>
             <ul class="clearfix">
-               <li>上海</li>
-               <li>北京</li>
-               <li>上海</li>
-               <li>北京</li>
-               <li>上海</li>
-               <li>北京</li>
-               <li>上海</li>
-               <li>北京</li>
+               <li v-for="item in hotList" :key="item.id">{{item.nm}}</li>
             </ul>
          </div>
-         <div class="city_sort">
-            <div>
-               <h2>A</h2>
+         <div class="city_sort" ref="city_sort">
+            <div v-for="item in cityList" :key="item.index">
+               <h2>{{item.index}}</h2>
                <ul>
-                  <li>阿拉善盟</li>
-                  <li>鞍山</li>
-                  <li>安庆</li>
-                  <li>安阳</li>
+                  <li v-for="list in item.list" :key="list.id">{{list.nm}}</li>
                </ul>
             </div>
-            <div>
-               <h2>B</h2>
-               <ul>
-                  <li>北京</li>
-                  <li>保定</li>
-                  <li>蚌埠</li>
-                  <li>包头</li>
-               </ul>
-            </div>
-            <div>
-               <h2>A</h2>
-               <ul>
-                  <li>阿拉善盟</li>
-                  <li>鞍山</li>
-                  <li>安庆</li>
-                  <li>安阳</li>
-               </ul>
-            </div>
-            <div>
-               <h2>B</h2>
-               <ul>
-                  <li>北京</li>
-                  <li>保定</li>
-                  <li>蚌埠</li>
-                  <li>包头</li>
-               </ul>
-            </div>
-            <div>
-               <h2>A</h2>
-               <ul>
-                  <li>阿拉善盟</li>
-                  <li>鞍山</li>
-                  <li>安庆</li>
-                  <li>安阳</li>
-               </ul>
-            </div>
-            <div>
-               <h2>B</h2>
-               <ul>
-                  <li>北京</li>
-                  <li>保定</li>
-                  <li>蚌埠</li>
-                  <li>包头</li>
-               </ul>
-            </div>	
+         </div>
+         <div class="city_index">
+            <ul>
+               <li v-for="(item, index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{item.index}}</li>
+            </ul>
          </div>
       </div>
-      <div class="city_index">
-         <ul>
-            <li>A</li>
-            <li>B</li>
-            <li>C</li>
-            <li>D</li>
-            <li>E</li>
-         </ul>
-      </div>
-   </div>
+  </div>
 </template>
 
 <script>
 export default {
-   name: 'City'
+   name: 'City',
+   data() {
+      return {
+         cityList: [],
+         hotList: []
+      }
+   },
+   methods: {
+      formatCityList: function(cities) {
+         var cityList = [];
+         var hotList = [];
+
+         for(var i = 0;i<cities.length;i++) {
+            if(cities[i].isHot === 1) {
+               hotList.push(cities[i]);
+            }
+         }
+         for(var i = 0; i < cities.length; i++) {
+            var firstLeter = cities[i].py.substring(0,1).toUpperCase();
+            if(toCom(firstLeter)) {//新添加index
+               cityList.push({index: firstLeter, list: [{nm: cities[i].nm, id: cities[i].id}]});
+            } else {
+               //累加索引
+               for(var j = 0; j < cityList.length; j++) {
+                  if(cityList[j].index === firstLeter) {
+                     cityList[j].list.push({nm:cities[i].nm,id:cities[i].id});
+                  }
+               }
+            }
+         }
+         cityList.sort((a,b) => {
+            if(a.index > b.index) {
+               return 1;
+            } else if(a.index < b.index) {
+               return -1;
+            } else {
+               return 0;
+            }
+         });
+         
+         function toCom(firstLeter) {
+            for(var i = 0; i < cityList.length; i++) {
+               if(cityList[i].index === firstLeter) {
+                  return false;
+               }
+            }
+            return true;
+         }
+         
+         return {
+            cityList,
+            hotList
+         }
+      },
+      handleToIndex: function(index) {
+         var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+          this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+      }
+   },
+   mounted() {
+      this.axios.get('/api/cityList').then((res)=>{
+         let msg = res.data.msg;
+         if(msg === 'ok') {
+            let data = res.data.data.cities;
+            var {cityList, hotList} = this.formatCityList(data);
+            this.cityList = cityList;
+            this.hotList = hotList;
+         }
+      })
+   }
 }
 </script>
 
 <style scoped>
 #content .city_body{ display: flex; width:100%; position: relative; top: 0;}
-.city_body .city_list{ flex:1;  background: #FFF5F0;}
+.city_body .city_list{ flex:1;  background: #FFF5F0;height: 100vh;overflow: auto}
 .city_body .city_list::-webkit-scrollbar{
     background-color:transparent;
     width:0;
